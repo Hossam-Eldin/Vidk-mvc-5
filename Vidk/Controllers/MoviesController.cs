3ee9a1493b26fdb84e76e79ee3f2bf2cb819cb46
Hossam-Elden <hosam.elden@hotmail.com>
2017-11-12 13:57:49 +0200

    working with database

---
+++ Vidk/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidk.Models;
+
+namespace Vidk.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var Movies = GetMovies();
+
+            return View(Movies);
+        }
+
+
+        private IEnumerable<Movies>  GetMovies()
+        {
+            return new List<Movies>
+                {
+                   new Movies { Id = 1, Name = "God Father" },
+                   new Movies{Id = 2, Name = "Lord of the rings"}
+                };
+        }
+    }
+}
