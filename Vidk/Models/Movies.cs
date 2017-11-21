4f010d58db05430ca37be2af20187d39a8be5b08
Hossam-Elden <hosam.elden@hotmail.com>
2017-11-21 15:26:33 +0200

    Movies Form Add new and Edit

--- Vidk/Models/Movies.cs
+++ Vidk/Models/Movies.cs
@@ -11,11 +11,18 @@ namespace Vidk.Models
         public int Id { get; set; }
         public string Name { get; set; }
         
-         [Required]
          public Genre Genre { get; set; }
-         public byte GenreId { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
          public DateTime DateAdded { get; set; }
-         public DateTime ReleaseDate { get; set; }
-         public byte NumberInStock { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
     }
 }
---
+++ Vidk/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidk.Models;
+
+namespace Vidk.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+         public Movies Movie { get; set; }
+         public string Title
+         {
+             get
+             {
+                 if (Movie != null && Movie.Id != 0)
+                     return "Edit Movie";
+ 
+                 return "New Movie";
+             }
+         }
+    }
+}
